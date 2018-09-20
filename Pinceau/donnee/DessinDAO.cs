1b43ca7d05cf08ee7e1ead41922d188c74ed751d
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-09-20 08:16:45 -0400

    [DATA] DAO pour le dessin en XML

---
+++ Pinceau/donnee/DessinDAO.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: stamandnadine
+ * Date: 09/18/2018
+ * Time: 17:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Pinceau.donnee
+{
+	/// <summary>
+	/// Description of DessinDAO.
+	/// </summary>
+	public class DessinDAO
+	{
+		public DessinDAO()
+		{
+		}
+	}
+}
